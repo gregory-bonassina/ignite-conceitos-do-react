a699c74b4e283d845ef98b874ce96f744b35e60c
Gregory Bonassina <bonassina3@gmail.com>
2021-11-02 17:25:58 -0300

    Implement functions to add, delete and complete task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if ( newTaskTitle.trim() === '' ) {
+      setNewTaskTitle('');
+      return;
+    } 
+    
+    const newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+      const newTasks = [...tasks, newTask ];
+
+      setTasks( newTasks );
+      setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map( task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks( newTasks );
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks( filteredTasks );
   }
 
   return (
